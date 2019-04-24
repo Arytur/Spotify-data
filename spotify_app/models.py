a6f44593a4463480fffe83b2de3514e3902bb79c
Artur Kozera <artur.kozera@gmail.com>
2019-04-24 23:28:21 +0200

    refactor album features

--- spotify_app/models.py
+++ spotify_app/models.py
@@ -67,26 +67,16 @@ class Album(models.Model):
 
     @staticmethod
     def calculate_album_features(spotify, tracks):
-        
-        tracks_number = len(tracks)
         dict_of_features = {'danceability': [], 'speechiness': [], 'acousticness': [],
                         'valence': [], 'instrumentalness': [], 'energy': [], 'liveness': []}
+        tracks_number = len(tracks)
+        
         for track in tracks:
             track = spotify.get_track_audio_features(track['id'])
-            dict_of_features['danceability'].append(track['danceability'])
-            dict_of_features['speechiness'].append(track['speechiness'])
-            dict_of_features['acousticness'].append(track['acousticness'])
-            dict_of_features['valence'].append(track['valence'])
-            dict_of_features['instrumentalness'].append(track['instrumentalness'])
-            dict_of_features['energy'].append(track['energy'])
-            dict_of_features['liveness'].append(track['liveness'])
+            for key in dict_of_features.keys():
+                dict_of_features[key].append(track[key])
 
-        dict_of_features['danceability'] = sum(dict_of_features['danceability']) / tracks_number
-        dict_of_features['speechiness'] = sum(dict_of_features['speechiness']) / tracks_number
-        dict_of_features['acousticness'] = sum(dict_of_features['acousticness']) / tracks_number
-        dict_of_features['valence'] = sum(dict_of_features['valence']) / tracks_number
-        dict_of_features['instrumentalness'] = sum(dict_of_features['instrumentalness']) / tracks_number
-        dict_of_features['energy'] = sum(dict_of_features['energy']) / tracks_number
-        dict_of_features['liveness'] = sum(dict_of_features['liveness']) / tracks_number
+        for key in dict_of_features.keys():
+            dict_of_features[key] = sum(dict_of_features[key]) / tracks_number
 
         return dict_of_features
