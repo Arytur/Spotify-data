0abe3c44a511b22f7412a4010dcd2a2fe711d63b
Artur Kozera <artur.kozera@gmail.com>
2019-11-18 23:13:56 +0100

    new models organization, refactor

--- spotify_app/models.py
+++ spotify_app/models.py
@@ -1,44 +1,46 @@
 from django.db import models
 
-from .tasks import SpotifyRequest
-
 
 class Track(models.Model):
-    track_id = models.CharField(max_length=32, unique=True)
-    track_artist = models.CharField(max_length=128)
-    track_name = models.CharField(max_length=128)
-    danceability = models.DecimalField(max_digits=4, decimal_places=3)
-    speechiness = models.DecimalField(max_digits=4, decimal_places=3)
-    acousticness = models.DecimalField(max_digits=4, decimal_places=3)
-    valence = models.DecimalField(max_digits=4, decimal_places=3)
-    instrumentalness = models.DecimalField(max_digits=4, decimal_places=3)
-    energy = models.DecimalField(max_digits=4, decimal_places=3)
-    liveness = models.DecimalField(max_digits=4, decimal_places=3)
+    """
+    Model for the single track.
+    """
+
+    id = models.CharField(max_length=32, unique=True)
+    name = models.CharField(max_length=128)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.track_artist + ' - ' + self.track_name
-
-    def get_features(self):
-        return [
-            self.danceability,
-            self.speechiness,
-            self.acousticness,
-            self.valence,
-            self.instrumentalness,
-            self.energy,
-            self.liveness
-        ]
-
-    def get_features_for_chart(self):
-        features = self.get_features()
-        return [int(feat * 100) for feat in features]
+        return f("{self.name} - {self.artist.name}")
 
 
 class Album(models.Model):
-    album_id = models.CharField(max_length=32, unique=True)
-    album_artist = models.CharField(max_length=128)
-    album_name = models.CharField(max_length=128)
-    album_image = models.URLField(null=True)
+    """
+    Model for the single album.
+    """
+
+    id = models.CharField(max_length=32, unique=True)
+    name = models.CharField(max_length=128)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    image = models.URLField(null=True)
+
+    def __str__(self):
+        return f("{self.name} - {self.artist.name}")
+
+
+class Artist(models.Model):
+    """
+    Model for the single artist.
+    """
+
+    name = models.CharField(max_length=128)
+
+
+class Features(models.Model):
+    """
+    additional features for track/albums
+    """
+
     danceability = models.DecimalField(max_digits=4, decimal_places=3)
     speechiness = models.DecimalField(max_digits=4, decimal_places=3)
     acousticness = models.DecimalField(max_digits=4, decimal_places=3)
@@ -46,9 +48,6 @@ class Album(models.Model):
     instrumentalness = models.DecimalField(max_digits=4, decimal_places=3)
     energy = models.DecimalField(max_digits=4, decimal_places=3)
     liveness = models.DecimalField(max_digits=4, decimal_places=3)
-    
-    def __str__(self):
-        return self.album_artist + ' - ' + self.album_name
 
     def get_features(self):
         return [
@@ -58,7 +57,7 @@ class Album(models.Model):
             self.valence,
             self.instrumentalness,
             self.energy,
-            self.liveness
+            self.liveness,
         ]
 
     def get_features_for_chart(self):
@@ -67,12 +66,19 @@ class Album(models.Model):
 
     @staticmethod
     def calculate_album_features(spotify, tracks):
-        dict_of_features = {'danceability': [], 'speechiness': [], 'acousticness': [],
-                        'valence': [], 'instrumentalness': [], 'energy': [], 'liveness': []}
+        dict_of_features = {
+            "danceability": [],
+            "speechiness": [],
+            "acousticness": [],
+            "valence": [],
+            "instrumentalness": [],
+            "energy": [],
+            "liveness": [],
+        }
         tracks_number = len(tracks)
-        
+
         for track in tracks:
-            track = spotify.get_track_audio_features(track['id'])
+            track = spotify.get_track_audio_features(track["id"])
             for key in dict_of_features.keys():
                 dict_of_features[key].append(track[key])
 
--- spotify_app/tasks.py
+++ spotify_app/tasks.py
@@ -2,57 +2,72 @@ import requests
 
 from .api_endpoints import API_ENDPOINTS, PLAYLISTS_URI
 
+
 def get_access_token(request):
-    return request.session.get('access_token')
+    return request.session.get("access_token")
 
 
-class SpotifyRequest():
-
+class SpotifyRequest:
     def __init__(self, request):
         self.access_token = get_access_token(request)
-        self.authorization_header = {"Authorization": "Bearer {}".format(self.access_token)}
+        self.authorization_header = {
+            "Authorization": "Bearer {}".format(self.access_token)
+        }
+
+    def _requests_url(self, url):
+        resp = requests.get(url, headers=self.authorization_header)
+        return resp.json()
 
     def get_users_recently_played(self):
-        url = API_ENDPOINTS['user_recently_played']
-        resp = self.requests_url(url)
-        return resp['items']
+        url = API_ENDPOINTS["user_recently_played"]
+        resp = self._requests_url(url)
+        return resp["items"]
 
     def get_new_releases(self):
-        url = API_ENDPOINTS['new_releases']
-        resp = self.requests_url(url)
-        return resp['albums']['items']
+        url = API_ENDPOINTS["new_releases"]
+        resp = self._requests_url(url)
+        return resp["albums"]["items"]
 
     def get_album(self, album_id):
-        url = API_ENDPOINTS['album'] + album_id
-        resp = self.requests_url(url)
+        url = API_ENDPOINTS["album"] + album_id
+        resp = self._requests_url(url)
         return resp
 
     def get_spotify_playlists(self):
 
         playlists_resp = {}
         for k, v in PLAYLISTS_URI.items():
-            url = API_ENDPOINTS['spotify_playlists'][0] + v + API_ENDPOINTS['spotify_playlists'][1]
-            resp = self.requests_url(url)
+            url = (
+                API_ENDPOINTS["spotify_playlists"][0]
+                + v
+                + API_ENDPOINTS["spotify_playlists"][1]
+            )
+            resp = self._requests_url(url)
             playlists_resp[k] = resp
 
         return playlists_resp
 
     def get_playlist_tracks(self, playlist_id):
-        url = API_ENDPOINTS['playlist_track'][0] + playlist_id + API_ENDPOINTS['playlist_track'][1]
-        return self.requests_url(url)
+        url = (
+            API_ENDPOINTS["playlist_track"][0]
+            + playlist_id
+            + API_ENDPOINTS["playlist_track"][1]
+        )
+        return self._requests_url(url)
 
     def get_track_audio_features(self, track_id):
-        url = API_ENDPOINTS['track_audio_feature'] + track_id
-        return self.requests_url(url)
+        url = API_ENDPOINTS["track_audio_feature"] + track_id
+        return self._requests_url(url)
 
     def search_result(self, searching):
-        url = API_ENDPOINTS['search'][0] + searching + API_ENDPOINTS['search'][1]
-        return self.requests_url(url)
+        url = API_ENDPOINTS["search"][0] + searching + API_ENDPOINTS["search"][1]
+        return self._requests_url(url)
 
     def artist_albums(self, artist):
-        url = API_ENDPOINTS['artist_albums'][0] + artist + API_ENDPOINTS['artist_albums'][1]
-        return self.requests_url(url)
+        url = (
+            API_ENDPOINTS["artist_albums"][0]
+            + artist
+            + API_ENDPOINTS["artist_albums"][1]
+        )
+        return self._requests_url(url)
 
-    def requests_url(self, url):
-        resp = requests.get(url, headers=self.authorization_header)
-        return resp.json()
