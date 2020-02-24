3a0c7f623089701d2883e5306496acc5342675a1
Artur Kozera <artur.kozera@gmail.com>
2020-02-24 20:52:54 +0100

    Create unit test for Artist, Features

--- spotify_app/tests/test_tasks.py
+++ spotify_app/tests/test_tasks.py
@@ -4,8 +4,52 @@ from django.http import HttpRequest
 from django.test import TestCase
 from unittest.mock import patch
 
-from spotify_app.models import Album, Artist, Track
-from spotify_app.tasks import create_track, create_album
+from spotify_app.models import (
+    Album,
+    Artist,
+    Features,
+    Track,
+    TrackFeatures
+    )
+from spotify_app.tasks import (
+    create_album,
+    create_artist,
+    create_track,
+    create_track_audio_features,
+)
+
+
+class TestArtistCreate(TestCase):
+    response = {
+        'artists': [
+            {
+                'id': '6ZLTlhejhndI4Rh53vYhrY',
+                'name': 'Ozzy Osbourne'
+            }
+        ]
+    }
+
+    def test_create_artist_from_response(self):
+
+        # check if Artist table is empty
+        self.assertFalse(Artist.objects.exists())
+
+        # create Artist and check if exists
+        create_artist(self.response)
+        self.assertTrue(Artist.objects.exists())
+
+    def test_not_create_same_artist_from_response(self):
+
+        # check if Artist is in the table
+        Artist.objects.create(
+            id='6ZLTlhejhndI4Rh53vYhrY',
+            name='Ozzy Osbourne'
+        )
+        self.assertEqual(Artist.objects.count(), 1)
+
+        # create Artist and check if exists
+        create_artist(self.response)
+        self.assertEqual(Artist.objects.count(), 1)
 
 
 class TestTrackCreate(TestCase):
@@ -22,6 +66,7 @@ class TestTrackCreate(TestCase):
     def test_create_track_for_existing_artist(self, mock_func):
         json_file = open('spotify_app/tests/fixtures/track_response_raw.json')
         mock_func.return_value = json.load(json_file)
+        # TODO: Use Request Factory
         request = HttpRequest()
 
         # create Artist and check if exists
@@ -37,6 +82,7 @@ class TestTrackCreate(TestCase):
     def test_create_track_for_new_artist(self, mock_func):
         json_file = open('spotify_app/tests/fixtures/track_response_raw.json')
         mock_func.return_value = json.load(json_file)
+        # TODO: Use Request Factory
         request = HttpRequest()
 
         # check if Artist table is empty
@@ -63,6 +109,7 @@ class TestAlbumCreate(TestCase):
     def test_create_album_for_existing_artist(self, mock_func):
         json_file = open('spotify_app/tests/fixtures/album_response_raw.json')
         mock_func.return_value = json.load(json_file)
+        # TODO: Use Request Factory
         request = HttpRequest()
 
         # create Artist and check if exists
@@ -78,6 +125,7 @@ class TestAlbumCreate(TestCase):
     def test_create_album_for_new_artist(self, mock_func):
         json_file = open('spotify_app/tests/fixtures/album_response_raw.json')
         mock_func.return_value = json.load(json_file)
+        # TODO: Use Request Factory
         request = HttpRequest()
 
         # check if Artist table is empty
@@ -88,3 +136,32 @@ class TestAlbumCreate(TestCase):
         self.assertEqual(Album.objects.first().id, self.album['id'])
         self.assertEqual(Album.objects.first().name, self.album['name'])
         self.assertEqual(Artist.objects.first().id, self.artist['id'])
+
+
+class TestFeaturesCreate(TestCase):
+    artist = dict(
+        id='6ZLTlhejhndI4Rh53vYhrY',
+        name='Ozzy Osbourne'
+    )
+
+    @patch('spotify_app.tasks.requests_url')
+    def test_create_track_audio_features(self, mock_func):
+        json_file = open('spotify_app/tests/fixtures/audio_features_raw.json')
+        mock_func.return_value = json.load(json_file)
+
+        # TODO: Use Request Factory
+        request = HttpRequest()
+
+        artist = Artist.objects.create(**self.artist)
+        track = Track.objects.create(
+            id='12345',
+            name='test track',
+            artist=artist
+        )
+        track_features = create_track_audio_features(request, track)
+        self.assertTrue(TrackFeatures.objects.exists())
+        self.assertTrue(Features.objects.exists())
+        self.assertEqual(TrackFeatures.objects.first().track, track)
+        self.assertEqual(TrackFeatures.objects.first().features, track_features)
+
+    # TODO: album audio features
