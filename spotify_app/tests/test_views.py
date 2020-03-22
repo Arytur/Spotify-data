52337b66212cca9f1a71f496b970d4af02ebea20
Artur Kozera <artur.kozera@gmail.com>
2020-03-22 20:02:29 +0100

    general refactor

--- spotify_app/tests/test_views.py
+++ spotify_app/tests/test_views.py
@@ -1,6 +1,7 @@
 import json
 from unittest.mock import patch
 
+from django.urls import reverse
 from django.test import TestCase
 
 from spotify_app.models import Features
@@ -26,25 +27,33 @@ def _add_access_token_to_client_session(client):
 class CallbackView(TestCase):
 
     def test_url_and_template(self):
-        response = self.client.get('/callback/q')
+        response = self.client.get(
+            reverse('callback')
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'callback.html')
 
-    def test_redirect_from_home_page_to_callback(self):
-        response = self.client.get('/')
+    def test_redirect_from_home_page_to_cllback(self):
+        response = self.client.get(
+            reverse('index')
+        )
 
         self.assertRedirects(response, '/callback/q')
 
-    @patch('spotify_app.tasks.requests_url')
+    @patch('spotify_app.views.get_new_releases')
     @patch('spotify_app.views.save_access_token_to_client_session')
     def test_redirect_from_callback_to_home_page(self, mock_save, mock_func):
         _add_access_token_to_client_session(self.client)
-        json_file = open('spotify_app/tests/fixtures/new_releases_raw.json')
-        resp_file = json.load(json_file)
-        mock_func.return_value = resp_file
+        self.albums = [
+            AlbumFactory()
+            for _ in range(10)
+        ]
+        mock_func.return_value = self.albums
 
-        response = self.client.get('/callback/q?code=ABCD123')
+        response = self.client.get(
+            '{}{}'.format(reverse('callback'), '?code=ABCD123')
+        )
 
         self.assertRedirects(response, '/')
 
@@ -61,7 +70,9 @@ class HomePageView(TestCase):
     @patch('spotify_app.views.get_new_releases')
     def test_url_and_template(self, mock_func):
         mock_func.return_value = self.albums
-        response = self.client.get('/', follow=True)
+        response = self.client.get(
+            reverse('index')
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
@@ -69,10 +80,23 @@ class HomePageView(TestCase):
     @patch('spotify_app.views.get_new_releases')
     def test_all_albums_in_html(self, mock_func):
         mock_func.return_value = self.albums
-        response = self.client.get('/', follow=True)
+        response = self.client.get(
+            reverse('index')
+        )
 
         for album in self.albums:
             self.assertContains(response, album.name)
+
+    @patch('spotify_app.views.get_new_releases')
+    def test_urls_for_all_albums_in_html(self, mock_func):
+        mock_func.return_value = self.albums
+        response = self.client.get(
+            reverse('index')
+        )
+
+        for album in self.albums:
+            album_url = reverse('album', args=[album.id])
+            self.assertContains(response, album_url)
 
 
 class UserRecentlyPlayedView(TestCase):
@@ -87,7 +111,9 @@ class UserRecentlyPlayedView(TestCase):
     @patch('spotify_app.views.get_user_recently_played')
     def test_url_and_template(self, mock_func):
         mock_func.return_value = self.tracks
-        response = self.client.get('/recently_played', follow=True)
+        response = self.client.get(
+            reverse('recently_played')
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'recently_played.html')
@@ -95,10 +121,23 @@ class UserRecentlyPlayedView(TestCase):
     @patch('spotify_app.views.get_user_recently_played')
     def test_all_tracks_in_html(self, mock_func):
         mock_func.return_value = self.tracks
-        response = self.client.get('/recently_played', follow=True)
+        response = self.client.get(
+            reverse('recently_played')
+        )
 
         for track in self.tracks:
             self.assertContains(response, track.name)
+
+    @patch('spotify_app.views.get_user_recently_played')
+    def test_urls_for_all_tracks_in_html(self, mock_func):
+        mock_func.return_value = self.tracks
+        response = self.client.get(
+            reverse('recently_played')
+        )
+
+        for track in self.tracks:
+            track_url = reverse('track', args=[track.id])
+            self.assertContains(response, track_url)
 
 
 class TrackTableView(TestCase):
@@ -120,19 +159,25 @@ class TrackTableView(TestCase):
         _add_access_token_to_client_session(self.client)
 
     def test_url_and_template(self):
-        response = self.client.get('/tracks_table/', follow=True)
+        response = self.client.get(
+            reverse('tracks_table')
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'tracks_table.html')
 
     def test_all_track_names_in_html(self):
-        response = self.client.get('/tracks_table/', follow=True)
+        response = self.client.get(
+            reverse('tracks_table')
+        )
 
         for track in self.tracks:
             self.assertContains(response, track.name)
 
     def test_all_artist_names_in_html(self):
-        response = self.client.get('/tracks_table/', follow=True)
+        response = self.client.get(
+            reverse('tracks_table')
+        )
 
         for track in self.tracks:
             self.assertContains(response, track.artist.name)
@@ -140,7 +185,9 @@ class TrackTableView(TestCase):
     def test_all_features_in_html(self):
         features_names = Features().get_fields_names
 
-        response = self.client.get('/tracks_table/', follow=True)
+        response = self.client.get(
+            reverse('tracks_table')
+        )
 
         for feat_name in features_names:
             self.assertContains(response, feat_name.capitalize())
@@ -148,7 +195,9 @@ class TrackTableView(TestCase):
     def test_all_features_values_in_html(self):
         features_names = Features().get_fields_names
 
-        response = self.client.get('/tracks_table/', follow=True)
+        response = self.client.get(
+            reverse('tracks_table')
+        )
 
         for feature in self.features:
             for feat_name in features_names:
@@ -175,19 +224,25 @@ class AlbumTableView(TestCase):
         _add_access_token_to_client_session(self.client)
 
     def test_url_and_template(self):
-        response = self.client.get('/albums_table/', follow=True)
+        response = self.client.get(
+            reverse('albums_table')
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'albums_table.html')
 
     def test_all_album_names_in_html(self):
-        response = self.client.get('/albums_table/', follow=True)
+        response = self.client.get(
+            reverse('albums_table')
+        )
 
         for album in self.albums:
             self.assertContains(response, album.name)
 
     def test_all_artist_names_in_html(self):
-        response = self.client.get('/albums_table/', follow=True)
+        response = self.client.get(
+            reverse('albums_table')
+        )
 
         for album in self.albums:
             self.assertContains(response, album.artist.name)
@@ -195,7 +250,9 @@ class AlbumTableView(TestCase):
     def test_all_features_in_html(self):
         features_names = Features().get_fields_names
 
-        response = self.client.get('/albums_table/', follow=True)
+        response = self.client.get(
+            reverse('albums_table')
+        )
 
         for feat_name in features_names:
             self.assertContains(response, feat_name.capitalize())
@@ -203,7 +260,9 @@ class AlbumTableView(TestCase):
     def test_all_features_values_in_html(self):
         features_names = Features().get_fields_names
 
-        response = self.client.get('/albums_table/', follow=True)
+        response = self.client.get(
+            reverse('albums_table')
+        )
 
         for feature in self.features:
             for feat_name in features_names:
@@ -221,25 +280,33 @@ class TrackDetailView(TestCase):
         _add_access_token_to_client_session(self.client)
 
     def test_url_and_template(self):
-        response = self.client.get(f'/track/{self.track.id}/')
+        response = self.client.get(
+            reverse('track', args=[self.track.id])
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'track.html')
 
     def test_track_name_in_html(self):
-        response = self.client.get(f'/track/{self.track.id}/')
+        response = self.client.get(
+            reverse('track', args=[self.track.id])
+        )
 
         self.assertContains(response, self.track.name)
 
     def test_artist_name_in_html(self):
-        response = self.client.get(f'/track/{self.track.id}/')
+        response = self.client.get(
+            reverse('track', args=[self.track.id])
+        )
 
         self.assertContains(response, self.track.artist.name)
 
     def test_all_features_in_html(self):
         features_names = Features().get_fields_names
 
-        response = self.client.get(f'/track/{self.track.id}/')
+        response = self.client.get(
+            reverse('track', args=[self.track.id])
+        )
 
         for feat_name in features_names:
             self.assertContains(response, feat_name.capitalize())
@@ -247,7 +314,9 @@ class TrackDetailView(TestCase):
     def test_all_features_values_in_html(self):
         features_names = Features().get_fields_names
 
-        response = self.client.get(f'/track/{self.track.id}/')
+        response = self.client.get(
+            reverse('track', args=[self.track.id])
+        )
 
         for feat_name in features_names:
             x_feat = getattr(self.features, feat_name)
@@ -256,7 +325,9 @@ class TrackDetailView(TestCase):
     def test_all_features_values_for_chart_in_html(self):
         features_values = self.features.get_features_for_chart()
 
-        response = self.client.get(f'/track/{self.track.id}/')
+        response = self.client.get(
+            reverse('track', args=[self.track.id])
+        )
 
         for numb in features_values:
             self.assertContains(response, numb)
@@ -265,7 +336,9 @@ class TrackDetailView(TestCase):
     def test_create_track_when_does_not_exist(self, mock_create_track):
         track_id = 'x233Ffs34kskzz'
 
-        self.client.get(f'/track/{track_id}/')
+        self.client.get(
+            reverse('track', args=[track_id])
+        )
 
         track = TrackFactory(id=track_id)
         track_and_features = TrackFeaturesFactory(track=track)
@@ -273,51 +346,76 @@ class TrackDetailView(TestCase):
 
         mock_create_track.assert_called_once()
 
-        response = self.client.get(f'/track/{track_id}/')
+        response = self.client.get(
+            reverse('track', args=[track_id])
+        )
         self.assertContains(response, track.name)
 
 
 class AlbumDetailView(TestCase):
 
     def setUp(self):
-        self.album_and_features = AlbumFeaturesFactory()
-        self.album = self.album_and_features.album
+        self.tracks = [TrackFactory() for _ in range(13)]
+        self.album = AlbumFactory.create(tracks=self.tracks)
+        self.album_and_features = AlbumFeaturesFactory.create(
+           album=self.album
+        )
         self.features = self.album_and_features.features
 
         _add_access_token_to_client_session(self.client)
 
     def test_url_and_template(self):
-        response = self.client.get(f'/album/{self.album.id}/')
+        response = self.client.get(
+            reverse('album', args=[self.album.id])
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'album.html')
 
     def test_album_name_in_html(self):
-        response = self.client.get(f'/album/{self.album.id}/')
+        response = self.client.get(
+            reverse('album', args=[self.album.id])
+        )
 
         self.assertContains(response, self.album.name)
 
+    def test_album_image_in_html(self):
+        response = self.client.get(
+            reverse('album', args=[self.album.id])
+        )
+
+        self.assertContains(response, self.album.image)
+
     def test_artist_name_in_html(self):
-        response = self.client.get(f'/album/{self.album.id}/')
+        response = self.client.get(
+            reverse('album', args=[self.album.id])
+        )
 
         self.assertContains(response, self.album.artist.name)
 
     def test_all_tracks_in_html(self):
-        tracks = [TrackFactory() for _ in range(13)]
-        album = AlbumFactory.create(tracks=tracks)
-        AlbumFeaturesFactory.create(
-           album=album
+        response = self.client.get(
+            reverse('album', args=[self.album.id])
         )
 
-        response = self.client.get(f'/album/{album.id}/')
-
-        for track in tracks:
+        for track in self.tracks:
             self.assertContains(response, track.name)
+
+    def test_urls_for_all_tracks_in_html(self):
+        response = self.client.get(
+            reverse('album', args=[self.album.id])
+        )
+
+        for track in self.tracks:
+            track_url = reverse('track', args=[track.id])
+            self.assertContains(response, track_url)
 
     def test_all_features_in_html(self):
         features_names = Features().get_fields_names
 
-        response = self.client.get(f'/album/{self.album.id}/')
+        response = self.client.get(
+            reverse('album', args=[self.album.id])
+        )
 
         for feat_name in features_names:
             self.assertContains(response, feat_name.capitalize())
@@ -325,25 +423,31 @@ class AlbumDetailView(TestCase):
     def test_all_features_values_in_html(self):
         features_names = Features().get_fields_names
 
-        response = self.client.get(f'/album/{self.album.id}/')
+        response = self.client.get(
+            reverse('album', args=[self.album.id])
+        )
 
-        for feat_name in features_names:
-            x_feat = getattr(self.features, feat_name)
-            self.assertContains(response, x_feat)
+        for feature in features_names:
+            x_feature = getattr(self.features, feature)
+            self.assertContains(response, x_feature)
 
     def test_all_features_values_for_chart_in_html(self):
         features_values = self.features.get_features_for_chart()
 
-        response = self.client.get(f'/album/{self.album.id}/')
+        response = self.client.get(
+            reverse('album', args=[self.album.id])
+        )
 
-        for numb in features_values:
-            self.assertContains(response, numb)
+        for feature_value in features_values:
+            self.assertContains(response, feature_value)
 
     @patch('spotify_app.views.create_album_tracks_and_features')
     def test_create_album_when_does_not_exist(self, mock_create_album):
         album_id = '123hkaCXX123kk'
 
-        self.client.get(f'/album/{album_id}/')
+        self.client.get(
+            reverse('album', args=[album_id])
+        )
 
         album = AlbumFactory(id=album_id)
         album_and_features = AlbumFeaturesFactory(album=album)
@@ -351,7 +455,9 @@ class AlbumDetailView(TestCase):
 
         mock_create_album.assert_called_once()
 
-        response = self.client.get(f'/album/{album_id}/')
+        response = self.client.get(
+            reverse('album', args=[album_id])
+        )
         self.assertContains(response, album.name)
 
 
@@ -369,7 +475,9 @@ class ArtistDetailView(TestCase):
     @patch('spotify_app.views.get_artist_and_albums')
     def test_url_and_template(self, mock_artist_albums):
         mock_artist_albums.return_value = self.artist.name, self.albums
-        response = self.client.get(f'/artist/{self.artist.id}/')
+        response = self.client.get(
+            reverse('artist', args=[self.artist.id])
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'artist.html')
@@ -377,17 +485,32 @@ class ArtistDetailView(TestCase):
     @patch('spotify_app.views.get_artist_and_albums')
     def test_artist_in_html(self, mock_artist_albums):
         mock_artist_albums.return_value = self.artist.name, self.albums
-        response = self.client.get(f'/artist/{self.artist.id}/')
+        response = self.client.get(
+            reverse('artist', args=[self.artist.id])
+        )
 
         self.assertContains(response, self.artist.name)
 
     @patch('spotify_app.views.get_artist_and_albums')
     def test_all_albums_in_html(self, mock_artist_albums):
         mock_artist_albums.return_value = self.artist.name, self.albums
-        response = self.client.get(f'/artist/{self.artist.id}/')
+        response = self.client.get(
+            reverse('artist', args=[self.artist.id])
+        )
 
         for album in self.albums:
             self.assertContains(response, album.name)
+
+    @patch('spotify_app.views.get_artist_and_albums')
+    def test_urls_for_all_albums_in_html(self, mock_artist_albums):
+        mock_artist_albums.return_value = self.artist.name, self.albums
+        response = self.client.get(
+            reverse('artist', args=[self.artist.id])
+        )
+
+        for album in self.albums:
+            album_url = reverse('album', args=[album.id])
+            self.assertContains(response, album_url)
 
 
 class SearchView(TestCase):
@@ -404,7 +527,9 @@ class SearchView(TestCase):
     def test_url_and_template(self, mock_search_results):
         mock_search_results.return_value = self.found_artists, '5'
 
-        response = self.client.get(f'/search/?q={self.searching_artist}', follow=True)
+        response = self.client.get(
+            '{}{}'.format(reverse('search'), f'?q={self.searching_artist}')
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'search.html')
@@ -413,17 +538,32 @@ class SearchView(TestCase):
     def test_found_searching_artist(self, mock_search_results):
         mock_search_results.return_value = self.found_artists, '5'
 
-        response = self.client.get(f'/search/?q={self.searching_artist}', follow=True)
+        response = self.client.get(
+            '{}{}'.format(reverse('search'), f'?q={self.searching_artist}')
+        )
         self.assertContains(response, self.searching_artist)
         self.assertContains(response, '5')
         for artist in self.found_artists:
             self.assertContains(response, artist.name)
 
     @patch('spotify_app.views.get_search_results')
+    def test_ulrs_for_found_searching_artist(self, mock_search_results):
+        mock_search_results.return_value = self.found_artists, '5'
+
+        response = self.client.get(
+            '{}{}'.format(reverse('search'), f'?q={self.searching_artist}')
+        )
+        for artist in self.found_artists:
+            artist_url = reverse('artist', args=[artist.id])
+            self.assertContains(response, artist_url)
+
+    @patch('spotify_app.views.get_search_results')
     def test_not_found_searching_artist(self, mock_search_results):
         mock_search_results.return_value = [], '0'
         searching_artist = f'not_{self.searching_artist}'
 
-        response = self.client.get(f'/search/?q={searching_artist}', follow=True)
+        response = self.client.get(
+            '{}{}'.format(reverse('search'), f'?q={searching_artist}')
+        )
         self.assertContains(response, searching_artist)
         self.assertContains(response, '0')
